87f551ad7e568261eecf851d8a8c4913743df846
primantaraputra <dianwildanp@gmail.com>
2025-06-27 10:56:26 +0700

    Add files via upload

---
+++ bismillah.py
@@ -0,0 +1,23 @@
+# pertama membuat variabel produk dengan menggunakan dictionary yang berfungsi untuk menyimpan produk seperti laptopd dll
+# kedua mmebuat user untuk menginput seperti nama, umur dll
+
+produk = {
+    "Laptop: 5500000",
+    "Smartphone 3000000",
+    "Tablet 2000000",
+    "Smartwatch 1500000",
+    "Headphone 500000"
+}
+
+def user():
+    print(f"Nama Pembeli:")
+    print(f"Umur pembeli:")
+    print(f"Apakah pelanggan member? ya/tidak:")
+    print(f"Gadget yang ingin di beli{produk}:")
+    print(f"Jumlah unit yang dibeli:")
+    print(f"Apakah ingin melakukan penawaran? ya/tidak:")
+
+
+
+
+print("Hello World!")
